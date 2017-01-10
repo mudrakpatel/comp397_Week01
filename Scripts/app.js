7cd81a7c4cfd35ff94174bc12d88334fef461601
mpate487 <300878960@cencol.ca>
2017-01-10 10:53:06 -0500

    Started coding app.js file for the logic

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,17 @@
 /**
  * Custom javascript goes here
  */
+(function () {
+    //variables
+    var answer;
+    var guesses = 0;
+    var hint = "";
+    /**
+     *@function randomNumber
+     *@params seed
+     *@return int 
+     */
+    function randomNumber(seed){
+        
+    }
+}());
